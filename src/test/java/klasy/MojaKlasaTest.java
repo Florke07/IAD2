651cefb8b256c8f32cb563eb8cf6db2f65b9397f
Wredter <wiktoriusrex@gmail.com>
2018-05-20 17:23:01 +0200

    Temp

--- src/test/java/klasy/MojaKlasaTest.java
+++ src/test/java/klasy/MojaKlasaTest.java
@@ -1,13 +1,21 @@
 package klasy;
 
-import org.junit.jupiter.api.Test;
+import network.Network;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MojaKlasaTest {
 
-    @Test
-    void test(){
-        assertTrue(true);
+    @org.junit.jupiter.api.Test
+    void out(){
+        ArrayList<Double> in = new ArrayList<>();
+        in.add(0.2);
+        in.add(0.2);
+        Network net = new Network(2,10);
+        System.out.println(net.toString());
+        net.sort(in);
+        System.out.println(net.toString());
     }
 }
