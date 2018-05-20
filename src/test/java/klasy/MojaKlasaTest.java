d0094859ecc7eedcccdee86050c5923e5e177a77
Florke07 <maciek.socha1@gmail.com>
2018-05-20 17:17:47 +0200

    update testów (i tak pewnie nie będziemy z tego kożystać, ale żeby było

--- src/test/java/klasy/MojaKlasaTest.java
+++ src/test/java/klasy/MojaKlasaTest.java
@@ -1,12 +1,13 @@
 package klasy;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MojaKlasaTest {
 
-    @org.junit.jupiter.api.Test
-    void out(){
-        MojaKlasa dupa = new MojaKlasa(8);
-        dupa.out();
+    @Test
+    void test(){
+        assertTrue(true);
     }
 }
